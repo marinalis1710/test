31d43b878c0cc85f258d24985bdaf78a3948eced
YuliaLis <44850179+YuliaLis@users.noreply.github.com>
2020-06-02 18:04:15 +0300

    Create populate.sql

---
+++ populate.sql
@@ -0,0 +1,10 @@
+INSERT INTO Human VALUES ('Свєта', 'Люлєловна', '18', 'Клієнт');
+INSERT INTO Human VALUES ('Юля', 'Лісова', '20', 'Волонтер');
+INSERT INTO Human VALUES ('Маріна', 'Лісіченко', '19', 'Клієнт');
+
+INSERT INTO Animal VALUES ('Мурчик', 'Кіт', '1', 'Хлопчик');
+INSERT INTO Animal VALUES ('Лунарія', 'Собака', '2', 'Дівчинка');
+INSERT INTO Animal VALUES ('Барбос', 'Собака', '4', 'Хлопчик');
+
+INSERT INTO HumanAnimal VALUES ('Свєта', 'Люлєловна','Мурчик', 'Кіт','Бере тваринку');
+INSERT INTO HumanAnimal VALUES ('Маріна', 'Лісіченко','Барбос', 'Собака','Шукає тваринку');
