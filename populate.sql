2f68b0deb9d1746a997057439db38b614796d7bf
YuliaLis <44850179+YuliaLis@users.noreply.github.com>
2020-06-02 18:31:29 +0300

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -1,10 +1,10 @@
-INSERT INTO Human VALUES ('Свєта', 'Люлєловна', '18', 'Клієнт');
-INSERT INTO Human VALUES ('Юля', 'Лісова', '20', 'Волонтер');
-INSERT INTO Human VALUES ('Маріна', 'Лісіченко', '19', 'Клієнт');
+INSERT INTO Human VALUES ('Свєта', 'Люлєловна', '18', 'Клієнт', 'Бере тваринку');
+INSERT INTO Human VALUES ('Юля', 'Лісова', '20', 'Волонтер','Працює');
+INSERT INTO Human VALUES ('Маріна', 'Лісіченко', '19', 'Клієнт','Шукає тваринку');
 
 INSERT INTO Animal VALUES ('Мурчик', 'Кіт', '1', 'Хлопчик');
 INSERT INTO Animal VALUES ('Лунарія', 'Собака', '2', 'Дівчинка');
 INSERT INTO Animal VALUES ('Барбос', 'Собака', '4', 'Хлопчик');
 
-INSERT INTO HumanAnimal VALUES ('Свєта', 'Люлєловна','Мурчик', 'Кіт','Бере тваринку');
-INSERT INTO HumanAnimal VALUES ('Маріна', 'Лісіченко','Барбос', 'Собака','Шукає тваринку');
+INSERT INTO HumanAnimal VALUES ('Свєта', 'Люлєловна', '18', 'Мурчик', 'Кіт');
+INSERT INTO HumanAnimal VALUES ('Маріна', 'Лісіченко', '19', 'Барбос', 'Собака');
